92860f131736c9065aa7314f8e5056a23d61362e
suesitran <tranhathuy@gmail.com>
2024-06-09 12:52:59 +1000

    UI for message box

---
+++ lib/widgets/message_box_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MessageBox extends StatefulWidget {
+  final ValueChanged<String> onSendMessage;
+  const MessageBox({required this.onSendMessage, super.key});
+
+  @override
+  State<MessageBox> createState() => _MessageBoxState();
+}
+
+class _MessageBoxState extends State<MessageBox> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          controller: _controller,
+          maxLines: 1,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.0),
+                borderSide:
+                    const BorderSide(color: Colors.black38, width: 1.0)),
+            contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
+            suffix: IconButton(
+              onPressed: () {
+                widget.onSendMessage(_controller.text);
+                _controller.text = '';
+              },
+              icon: const Icon(Icons.send),
+            ),
+          ),
+          onSubmitted: (value) {
+            widget.onSendMessage(value);
+            _controller.text = '';
+          },
+        ),
+      );
+}
