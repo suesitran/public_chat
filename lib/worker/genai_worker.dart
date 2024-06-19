84d91071fa637daad3fb74f372b8af79a1cb5fd2
suesitran <tranhathuy@gmail.com>
2024-06-19 19:45:25 +1000

    Add Unit test for GenAIWorker

--- lib/worker/genai_worker.dart
+++ lib/worker/genai_worker.dart
@@ -3,20 +3,18 @@ import 'dart:async';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class GenAIWorker {
-  late final GenerativeModel _model;
+  late final GenerativeModelWrapper _model;
 
   final List<ChatContent> _content = [];
   final StreamController<List<ChatContent>> _streamController = StreamController.broadcast();
 
   Stream<List<ChatContent>> get stream => _streamController.stream;
 
-  GenAIWorker() {
-    const apiKey = String.fromEnvironment('apiKey');
-
-    _model = GenerativeModel(model: 'gemini-1.5-pro', apiKey: apiKey);
+  GenAIWorker({GenerativeModelWrapper? wrapper}) {
+    _model = wrapper ?? GenerativeModelWrapper();
   }
 
-  void sendToGemini(String message) async {
+  Future<void> sendToGemini(String message) async {
     _content.add(ChatContent.user(message));
     _streamController.sink.add(_content);
 
@@ -30,11 +28,11 @@ class GenAIWorker {
       } else {
         _content.add(ChatContent.gemini(text));
       }
-
-      _streamController.sink.add(_content);
     } catch (e) {
       _content.add(ChatContent.gemini('Unable to generate response'));
     }
+
+    _streamController.sink.add(_content);
   }
 }
 
@@ -46,4 +44,16 @@ class ChatContent {
 
   ChatContent.user(this.message) : sender = Sender.user;
   ChatContent.gemini(this.message) : sender = Sender.gemini;
+}
+
+class GenerativeModelWrapper {
+  late final GenerativeModel _model;
+
+  GenerativeModelWrapper() {
+    const apiKey = String.fromEnvironment('apiKey');
+
+    _model = GenerativeModel(model: 'gemini-1.5-pro', apiKey: apiKey);
+  }
+
+  Future<GenerateContentResponse> generateContent(Iterable<Content> prompt) => _model.generateContent(prompt);
 }
