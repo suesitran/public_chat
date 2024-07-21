c2f3ed63b8ac4493d9db1589dbc65a2c5883a494
Suesi Tran <17781268+suesitran@users.noreply.github.com>
2024-07-21 12:19:04 +1000

    Chat session (#2)

--- lib/repository/genai_model.dart
+++ lib/repository/genai_model.dart
@@ -2,13 +2,15 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 
 class GenAiModel {
   late final GenerativeModel _model;
+  late final ChatSession _session;
 
   GenAiModel() {
     const apiKey = String.fromEnvironment('apiKey');
 
     _model = GenerativeModel(model: 'gemini-1.5-pro', apiKey: apiKey);
+    _session = _model.startChat();
   }
 
-  Future<GenerateContentResponse> sendMessage(List<Content> content) =>
-      _model.generateContent(content);
+  Future<GenerateContentResponse> sendMessage(Content content) =>
+      _session.sendMessage(content);
 }
