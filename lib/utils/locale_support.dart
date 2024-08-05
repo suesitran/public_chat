4e621226febeed4a4dc7d189a62a18c45061d388
Suesi Tran <17781268+suesitran@users.noreply.github.com>
2024-08-05 13:13:18 +0700

    Add localisation (#4)
    * add localisation
    
    * add missing gitignore
    
    * Add vietnamese
    
    * Add locale support to BuildContext
    
    * Update test
    
    * Test locale, and format code

---
+++ lib/utils/locale_support.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+extension LocaleSupport on BuildContext {
+  AppLocalizations get locale {
+    AppLocalizations? locale = AppLocalizations.of(this);
+
+    if (locale == null) {
+      throw Exception('Locale is requested before Localisation initalised');
+    }
+
+    return locale;
+  }
+}
--- test/material_wrapper_extension.dart
+++ test/material_wrapper_extension.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension MaterialWrapperExtension on WidgetTester {
-  Future<void> wrapAndPump(Widget widget) => pumpWidget(MaterialApp(
+  Future<void> wrapAndPump(Widget widget, {Locale? locale}) =>
+      pumpWidget(MaterialApp(
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: locale,
         home: Scaffold(
           body: widget,
         ),
