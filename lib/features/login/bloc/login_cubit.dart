59586f77c66822cf9f112296ea28a0d4f26c27f2
Suesi Tran <17781268+suesitran@users.noreply.github.com>
2024-09-06 17:38:38 +1000

    Part8 firebase authentication (#5)

---
+++ lib/features/login/bloc/login_cubit.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:public_chat/utils/bloc_extensions.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial()) {
+    userSubscription = googleSignIn.onCurrentUserChanged.listen((user) {
+      if (user != null) {
+        _authenticateToFirebase(user);
+      }
+    },);
+  }
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  late final StreamSubscription userSubscription;
+
+  void requestLogin() async {
+    GoogleSignInAccount? googleUser;
+    try {
+      googleUser = await googleSignIn.signIn();
+    } on PlatformException catch (e) {
+      emitSafely(LoginFailed(e.toString()));
+      return null;
+    }
+
+    if (googleUser == null) {
+      emitSafely(LoginFailed('User cancelled'));
+      return null;
+    }
+
+    await _authenticateToFirebase(googleUser);
+  }
+
+  Future<void> _authenticateToFirebase(GoogleSignInAccount googleUser) async {
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final OAuthCredential oAuthCredential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    try {
+      final UserCredential userCredential =
+          await firebaseAuth.signInWithCredential(oAuthCredential);
+      final User? user = userCredential.user;
+
+      if (user == null) {
+        emitSafely(LoginFailed('Unable to get user credential'));
+
+        return null;
+      }
+
+      emitSafely(LoginSuccess(user.displayName ?? 'Unknown display name'));
+    } on FirebaseAuthException catch (e) {
+      emitSafely(LoginFailed(e.toString()));
+      return null;
+    } catch (e) {
+      emitSafely(LoginFailed(e.toString()));
+      return null;
+    }
+  }
+  @override
+  Future<void> close() {
+    userSubscription.cancel();
+    return super.close();
+  }
+}
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:public_chat/features/login/bloc/login_cubit.dart';
+import 'package:public_chat/features/login/ui/widgets/sign_in_button.dart';
 import 'package:public_chat/utils/locale_support.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: TextButton(
-            onPressed: () {
-              // TODO login with Google
-            },
-            child: Text(context.locale.login),
-          ),
-        ),
+  Widget build(BuildContext context) =>
+      BlocProvider<LoginCubit>(
+        create: (context) => LoginCubit(),
+        child: const _LoginScreenBody(),
       );
+}
+
+class _LoginScreenBody extends StatelessWidget {
+  const _LoginScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Center(
+      child: BlocBuilder<LoginCubit, LoginState>(
+        builder: (context, state) {
+          if (state is LoginSuccess) {
+            return Text('Hello ${state.displayName}');
+          }
+
+          if (state is LoginFailed) {
+            return Column(
+              children: [
+                Text('Login failed. Try again'),
+                buildSignInButton(label: context.locale.login, onPressed: () => context.read<LoginCubit>().requestLogin(),)
+              ],
+            );
+          }
+
+          return buildSignInButton(label: context.locale.login, onPressed: () => context.read<LoginCubit>().requestLogin(),);
+
+        },
+      )
+    ),
+  );
 }
