db5ef1492998c98c90cfb8986773a46df8d38e99
suesitran <17781268+suesitran@users.noreply.github.com>
2024-09-24 09:06:03 +1000

    Draft - Add cloud function

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,27 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const v2 = require("firebase-functions/v2");
+const vertexAIApi = require("@google-cloud/vertexai");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const project = 'your-cloud-project';
+const location = 'us-central1';
+const textModel =  'gemini-1.0-pro';
+const visionModel = 'gemini-1.0-pro-vision';
+
+const vertexAI = new vertexAIApi.VertexAI({project: project, location: location});
+
+const generativeVisionModel = vertexAI.getGenerativeModel({
+    model: visionModel,
+});
+
+const generativeModelPreview = vertexAI.preview.getGenerativeModel({
+    model: textModel,
+});
+
+exports.onChatWritten = v2.firestore.onDocumentWritten("/public/{messageId}", (event) => {
+    console.log('chat written: ${messageId}');
+})
