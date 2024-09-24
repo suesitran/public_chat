f79aa73d40c6b6d8bddb72aefe4dbaaee72c618a
suesitran <17781268+suesitran@users.noreply.github.com>
2024-09-24 12:25:56 +1000

    read message from data, and send to Gemini to translate

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,7 @@ const vertexAIApi = require("@google-cloud/vertexai");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-const project = 'your-cloud-project';
+const project = 'proj-atc';
 const location = 'us-central1';
 const textModel =  'gemini-1.0-pro';
 const visionModel = 'gemini-1.0-pro-vision';
@@ -27,6 +27,24 @@ const generativeModelPreview = vertexAI.preview.getGenerativeModel({
     model: textModel,
 });
 
-exports.onChatWritten = v2.firestore.onDocumentWritten("/public/{messageId}", (event) => {
-    console.log('chat written: ${messageId}');
+exports.onChatWritten = v2.firestore.onDocumentWritten("/public/{messageId}",async (event) => {
+    const document = event.data.after.data();
+    const message = document["message"];
+    console.log('message: ', message);
+
+    const request = {
+        contents: [
+            {
+                "role": "user",
+                parts: [
+                    {
+                        text: `translate this text to English: ${message}`
+                    }
+                ]
+            }
+        ]
+    }
+    const result = await generativeModelPreview.generateContent(request);
+    const response = result.response;
+    console.log('Response:', JSON.stringify(response));
 })
