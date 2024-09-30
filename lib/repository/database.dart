dc8e6a4c6516291262971ca730cf4c694a57f2ee
suesitran <17781268+suesitran@users.noreply.github.com>
2024-09-30 10:23:05 +1000

    remove unused import

--- lib/repository/database.dart
+++ lib/repository/database.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:public_chat/_shared/data/chat_data.dart';
 
 final class Database {
