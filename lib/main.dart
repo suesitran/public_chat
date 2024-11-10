8c26539b065ae9668cbd0a5799ef6c7576542c67
bangindong <qhuy.2207@gmail.com>
2024-11-10 22:08:12 +0700

    add firebase emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,6 +14,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   ServiceLocator.instance.initialise();
 
   runApp(BlocProvider<GenaiBloc>(
